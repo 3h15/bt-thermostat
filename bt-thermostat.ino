7231a2e2ed14ee0ac7fac937fc98f999fd848bb6
Cyprien Poisson <c.poisson@3h15.com>
2015-10-14 18:16:10 +0200

    Add BT.

--- bt-thermostat.ino
+++ bt-thermostat.ino
@@ -1,5 +1,6 @@
 #include <Wire.h>
 #include "Adafruit_HDC1000.h"
+#include <RFduinoGZLL.h>
 
 struct SensorData {
   // Keep under 20 bytes!
@@ -8,6 +9,9 @@ struct SensorData {
   float humidity;  // 4 bytes
   // NOTE: compiler will reserve a byte here not defined
 };
+
+device_t role = DEVICE0;
+bool isSending = false;
 
 Adafruit_HDC1000 hdc;
 
@@ -21,6 +25,16 @@ void setup(){
 
   // Vcc on pin 3 for HTC sensor
   pinMode(6, OUTPUT);
+
+  // Max power
+  RFduinoGZLL.txPowerLevel = +4;
+
+  // Change addresses to avoid conflicts with other networks.
+  // (note: the msb cannot be 0x55 or 0xaa)
+  RFduinoGZLL.hostBaseAddress = 0x4A27E98F;
+  RFduinoGZLL.deviceBaseAddress = 0xF487EE4A;
+
+
 
 }
 
@@ -37,9 +51,36 @@ void loop() {
     Serial.println("NO SENSOR?");
   }
 
+  sendToHost(data);
+
   Serial.print("Loop Duration: ");
   Serial.println(millis() - loopStartTime);
   RFduino_ULPDelay(2000);
+}
+
+
+void sendToHost(struct SensorData data){
+  // Start GZLL
+  RFduinoGZLL.begin(role);
+
+  // Send data
+  isSending = true;
+  RFduinoGZLL.sendToHost((char *)&data, sizeof(data));
+
+  // Wait max 10 seconds for ACK
+  int startTime = millis();
+  while(isSending && ( millis()-startTime < 10000)){
+    delay(10);
+  }
+
+  // Stop GZLL
+  RFduinoGZLL.end();
+}
+
+void RFduinoGZLL_onReceive(device_t device, int rssi, char *data, int len)
+{
+  // Reset sending state.
+  isSending = false;
 }
 
 
