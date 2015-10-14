335796e01a8079815e0b93e3e9d3f6d2f62979f1
Cyprien Poisson <c.poisson@3h15.com>
2015-10-14 16:08:50 +0200

    HDC1000 reading basics.

--- bt-thermostat.ino
+++ bt-thermostat.ino
@@ -1,12 +1,35 @@
+#include <Wire.h>
+#include "Adafruit_HDC1000.h"
 
+
+Adafruit_HDC1000 hdc;
 
 void setup(){
 
   Serial.begin(9600);
 
   Serial.println("START");
+
+  // Vcc on pin 3 for HTC sensor
+  pinMode(3, OUTPUT);
+  digitalWrite(3, HIGH);
+
+  // Gnd on pin 4 for HTC sensor
+  pinMode(4, OUTPUT);
+  digitalWrite(4, LOW);
+
+  hdc = Adafruit_HDC1000();
+
+  if (!hdc.begin()) {
+    Serial.println("NO SENSOR?");
+    while (1);
+  }
 }
 
-void loop(){
-
+void loop() {
+  Serial.print("T: ");
+  Serial.print(hdc.readTemperature());
+  Serial.print("\t\tH: ");
+  Serial.println(hdc.readHumidity());
+  delay(2000);
 }
