0e305963d55c248a0b7277e300d881b678aed5b6
Cyprien Poisson <c.poisson@3h15.com>
2015-10-14 16:36:38 +0200

    Better loop.
    - Add timings to keep an eye on power costs.
    - Switch sensor off after reading.

--- bt-thermostat.ino
+++ bt-thermostat.ino
@@ -8,28 +8,32 @@ void setup(){
 
   Serial.begin(9600);
 
-  Serial.println("START");
-
-  // Vcc on pin 3 for HTC sensor
-  pinMode(6, OUTPUT);
-  digitalWrite(6, HIGH);
-
   // Gnd on pin 4 for HTC sensor
   pinMode(5, OUTPUT);
   digitalWrite(5, LOW);
 
-  hdc = Adafruit_HDC1000();
+  // Vcc on pin 3 for HTC sensor
+  pinMode(6, OUTPUT);
 
-  if (!hdc.begin()) {
-    Serial.println("NO SENSOR?");
-    while (1);
-  }
 }
 
 void loop() {
-  Serial.print("T: ");
-  Serial.print(hdc.readTemperature());
-  Serial.print("\t\tH: ");
-  Serial.println(hdc.readHumidity());
-  delay(2000);
+  int loopStartTime = millis();
+
+  // Turn sensor on.
+  digitalWrite(6, HIGH);
+
+  hdc = Adafruit_HDC1000();
+
+  if (hdc.begin()) {
+    Serial.println(hdc.readTemperature());
+    Serial.println(hdc.readHumidity());
+  }
+
+  // Turn sensor off.
+  digitalWrite(6, HIGH);
+
+  Serial.print("Loop Duration: ");
+  Serial.println(millis() - loopStartTime);
+  RFduino_ULPDelay(2000);
 }
